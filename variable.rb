d10c767f8d24c941223092f2a195fda53a8ac1fa
Lauren Stone <lstone321@gmail.com>
2020-07-07 01:31:09 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,4 +1,6 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-describe #greeting
-
+describe "#greeting"
+greeting = "Hello World"
+learn submit 
+end 
